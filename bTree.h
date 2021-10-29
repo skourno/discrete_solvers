3dc541586d7e156f2ce2969b8fefaf88aee88938
Spiros Kournopoulos <skourno@gmail.com>
2021-10-29 17:52:29 +0100

    basic template Node implementation

--- bTree.h
+++ bTree.h
@@ -3,7 +3,28 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+namespace bTree
+{
+    template<class T>
+    struct Node
+    {
+        T *data;
+        Node* left;
+        Node* right;
 
+        Node(T &data_in)
+        {
+            data  = &data_in;
+            left  = nullptr;
+            right = nullptr;
+        }
+    };
 
+    template<class T>
+    class bTree {
+        private:
+        Node<T>* _head;
+    };
+}
 
 #endif
