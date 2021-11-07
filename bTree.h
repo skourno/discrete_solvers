6dedcdd2a968b460dfcbe1a4b79ec1a074720fdc
Spiros Kournopoulos <skourno@gmail.com>
2021-11-07 13:37:53 +0000

    add left/right node functions

--- bTree.h
+++ bTree.h
@@ -24,12 +24,30 @@ namespace bTree
         T get_data() {
             return *data;
         }
+
+        void add_node_left (Node<T>* node) {
+            left = node;
+        }
+
+        void add_node_right (Node<T>* node) {
+            right = node;
+        }
     };
 
     template<class T>
     class bTree {
         private:
         Node<T>* _head;
+
+        public:
+        bTree () {
+            _head = nullptr;
+        }
+        bTree (Node<T>* head) : _head(head) {}
+
+        Node<T>* get_head() {
+            return _head;
+        }
     };
 }
 
